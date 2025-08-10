9c184bc7a323099649d23aac0dccb8de58e7d68e
Kireev Mikle <kireev.mikle@gmail.com>
2025-08-10 11:34:53 +0100

    add scedule and grade models

---
+++ scedule/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+    summary = models.TextField()
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Class(models.Model):
+    title = models.CharField(max_length=100)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    summary = models.TextField()
+
+class Schedule(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    class_room = models.ForeignKey(Class, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.IntegerField()
+    date = models.DateField()
